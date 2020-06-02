335e82ff381b0cb857973b9792c45b06b3584c3b
Eduardo Rodriguez <erodrig@us.ibm.com>
2020-06-02 16:09:39 -0500

    Removing OIDC install, not needed with newer Liberty versions.

--- WebApplication/run_locally.sh
+++ WebApplication/run_locally.sh
@@ -11,10 +11,6 @@ cp ../Liberty/server.xml target/liberty/wlp/usr/servers/sample
 cp ../Liberty/mytruststore.jks target/liberty/wlp/usr/servers/sample
 cp ../Liberty/apps/libertySample-1.0.0.war target/liberty/wlp/usr/servers/sample/apps 
 
-# Run the OICD installer
-
-./target/liberty/wlp/bin/installUtility install openidconnectclient-1.0 --acceptLicense 
-
 # Delete existing key.jks file and create new key.jks file
 
 rm target/liberty/wlp/usr/servers/sample/resources/security/key.p12
